51bcb8da4bbc987d53cc4518ce7224c3e8602fbb
jorchsg <sandoval.jorge.407@gmail.com>
2020-07-31 16:10:34 -0700

    Centralizando las peticiones HTTP

--- src/app/services/spotify.service.ts
+++ src/app/services/spotify.service.ts
@@ -11,35 +11,33 @@ import { map } from 'rxjs/operators';
 export class SpotifyService {
 
   constructor(private http: HttpClient) {
-    console.log('spotify Service Listo');
+    console.log('Spotify Service Ready');
+  }
+
+  //Create main query for NewReleases and Get Artists functions
+  getQuery(query: string) {
+    const url = `https://api.spotify.com/v1/${query}`;
+
+    const headers = new HttpHeaders({
+      //Bearer: Expires each hour, we have to request a new post for each expirtation
+      'Authorization': 'Bearer BQCb6YcMYhiv1RK_iSGezWZcKV3iiCMu-Si5uWFTkbJkUwfQX1FlQwx3dG2UCnN2btJz6PTNd4BFnb14nXU'
+    });
+
+    return this.http.get(url, { headers });
   }
 
   getNewReleases() {
-
-    const headers = new HttpHeaders({
-      //Bearer: Expires each hour, we have to request a new post for each expirtation
-      'Authorization': 'Bearer BQCb6YcMYhiv1RK_iSGezWZcKV3iiCMu-Si5uWFTkbJkUwfQX1FlQwx3dG2UCnN2btJz6PTNd4BFnb14nXU'
-    });
-
-    return this.http.get('https://api.spotify.com/v1/browse/new-releases?limit=20', { headers })
+    return this.getQuery('browse/new-releases?limit=20')
       .pipe(map(data => {
         return data['albums'].items; //Way to get the data more directly using "pipe" and "map" instance "subscribe"
       }));
-
   }
 
   getArtist(termino: string) {
-
-    const headers = new HttpHeaders({
-      //Bearer: Expires each hour, we have to request a new post for each expirtation
-      'Authorization': 'Bearer BQCb6YcMYhiv1RK_iSGezWZcKV3iiCMu-Si5uWFTkbJkUwfQX1FlQwx3dG2UCnN2btJz6PTNd4BFnb14nXU'
-    });
-
-    return this.http.get(`https://api.spotify.com/v1/search?q=${termino}&type=artist&limit=15`, { headers })
+    return this.getQuery(`search?q=${termino}&type=artist&limit=15`)
       .pipe(map(data => {
         return data['artists'].items;//Way to get the data more directly using "pipe" and "map" instance "subscribe"
       }));
-
   }
 }
 
